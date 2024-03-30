b26e4cdd7869ae0c0d924532c2ba1d9b053bbd4f
Tyras <andreicristian.vlad@gmail.com>
2024-03-30 16:08:51 +0200

    updated country regex retreival inside postcodeExtractor

--- Extractors/postcodeExtractor.js
+++ Extractors/postcodeExtractor.js
@@ -1,9 +1,9 @@
-function findPostcode(text, country) {
+function findPostcode(text, countryRegex, Country = null) {
     //Things to check for: 
     // some states are spelled fully, such as Ohio instead of OH
     // could return country based on postal code in most cases
-    let postalcodeFormat = new RegExp(country);
-    // if(!country) {
+    let postalcodeFormat = new RegExp(countryRegex);
+    // if(!countryRegex) {
     //     for(let zipFormat in countries){
     //         postalcodeFormat = postalcodeRegex[zipFormat];
     //         let postcodeMatch = text.match(postalcodeFormat);
@@ -14,30 +14,30 @@ function findPostcode(text, country) {
     //     }
     // } else {
 
-        const postcodeWithLabelRegex = /Postcode\s*([A-Z]{2}\s*\d{4,10})\b/;
-        const postcodeRegexWithState = /\b[A-Z]{2}\s*(\d{5})\b/; // capture only the postcode part
-        
-        let postcodeMatch = postalcodeFormat ? postalcodeFormat.exec(text) : null;
-        postcode = postcodeMatch && postcodeMatch[1] ? postcodeMatch[1] : null;
+    const postcodeWithLabelRegex = /Postcode\s*([A-Z]{2}\s*\d{4,10})\b/;
+    const postcodeRegexWithState = /\b[A-Z]{2}\s*(\d{5})\b/; // capture only the postcode part
     
-        if(!postcode) {
-            postcodeMatch = postcodeWithLabelRegex.exec(text);
+    let postcodeMatch = postalcodeFormat ? postalcodeFormat.exec(text) : null;
+    postcode = postcodeMatch && postcodeMatch[1] ? postcodeMatch[1] : null;
+
+    if(!postcode) {
+        postcodeMatch = postcodeWithLabelRegex.exec(text);
+        if (postcodeMatch && postcodeMatch[1]) {
+            postcode = postcodeMatch[1];
+        } else {
+            postcodeMatch = postcodeRegexWithState.exec(text);
             if (postcodeMatch && postcodeMatch[1]) {
-                postcode = postcodeMatch[1];
-            } else {
-                postcodeMatch = postcodeRegexWithState.exec(text);
-                if (postcodeMatch && postcodeMatch[1]) {
-                    postcode = postcodeMatch[1].trim(); 
-                }
-                if (!postcode) {
-                    const genericPostcodeRegex = /\b\d{5}\b/;
-                    const genericPostcodeMatch = genericPostcodeRegex.exec(text);
-                    if (genericPostcodeMatch && genericPostcodeMatch[0]) {
-                        postcode = genericPostcodeMatch[0].trim();
-                    }
+                postcode = postcodeMatch[1].trim(); 
+            }
+            if (!postcode) {
+                const genericPostcodeRegex = /\b\d{5}\b/;
+                const genericPostcodeMatch = genericPostcodeRegex.exec(text);
+                if (genericPostcodeMatch && genericPostcodeMatch[0]) {
+                    postcode = genericPostcodeMatch[0].trim();
                 }
             }
         }
+    }
     // }
 
   return postcode;
