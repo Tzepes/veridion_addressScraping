ebf7dea5e7e295a6dc8373ffc147b39ea9af9c3b
Tyras <andreicristian.vlad@gmail.com>
2024-04-03 20:20:22 +0300

    code cleanup

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const brightusername = fs.readFileSync('./brightdata/.brightdataUsername').toStr
 const brightpassword = fs.readFileSync('./brightdata/.brightdataPassword').toString().trim();
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const {countries, countryAbbreviations, getCountryAbbreviation} = require('./countriesCodes.js');
+const {countries, getCountryAbbreviation} = require('./countriesCodes.js');
 
 const {findCountry, getCountryFromURL} = require('./Extractors/countryExtractor.js');
 const {loopForPostcodeIfCountry} = require('./Extractors/postcodeExtractor.js');
@@ -16,7 +16,7 @@ const getPostalCodeFormat = require('./postalcodeRegex.js');
 
 // Declare routes:
 
-const routes = ['/contact', '/about', '/contact-us', '/about-us', '/contactus', '/aboutus', '/contact-us.html', '/about-us.html', '/contactus.html', '/aboutus.html', '/contact.html', '/about.html', '/locations'];
+const routes = ['/contact', '/about'];
 
 const csvWriter = createCsvWriter({
     path: 'results/linkResultsTable.csv',
@@ -53,64 +53,38 @@ const axiosBrightDataInstance = axios.create({
     while(record = await cursor.next()) {
         // console.log("");
         // console.log(record.domain) // returns the URL
-        let response;
-        try {
-            response = await axios.get('http://' + record.domain, {
-                timeout: 10000
-            });
-            console.log("");
-            if (response.status === 200) {
-                console.log(record.domain)
-                retreivedData = retrieveLocationData(response.data, record.domain);
-            } else {
-                console.log('Failed');
-            }
-        } catch (error) {
-            console.log('axios error connection');
-        }
-
-        // if not enough info (for example, no postcode)
-            // try other routes
-                // if postcode found 
-                    // break
-            //if no response from other routes
-                // return at least country by url and road (remember the data durring route looping)
-
-        if(response && !retreivedData.postcode){
-            checkRoutes('http://' + record.domain, retreivedData)
-        }
+        let retreivedData = await accessDomain('http://' + record.domain);
+        writeCSV(retreivedData, record.domain);
     }
     console.log("");
 
     await reader.close();
 })();
 
-async function checkRoutes(domain, retreivedData) {
-    for (const route of routes) {
-        try {
-            const response = await axios.get('http://' + domain + route, {
-                timeout: 10000
-            });
-            console.log("");
-            if (response.status === 200) {
-                console.log(domain + route)
-                retreivedData = retrieveLocationData(response.data, domain + route);
-            } else {
-                console.log('Failed');
-            }
-        } catch (error) {
-            //console.log('axios error connection');
-        }
+async function accessDomain(domain){
+    let response;
+    let retreivedData = null;
 
-        if(retreivedData.postcode){
-            break;
+    try {
+        response = await axios.get(domain, {
+            timeout: 2000
+        });
+        console.log("");
+        if (response.status === 200) {
+            console.log(domain)
+            retreivedData = retrieveLocationData(response.data, domain);
+        } else {
+            console.log('Failed');
         }
+    } catch (error) {
+        console.log('axios error connection');
     }
+
+    return retreivedData;
 }
 
 async function retrieveLocationData(htmlContent, url) {
     let country;
-    let countryGotFromURL = false;
     let region;
     let city;
     let postcode;
@@ -126,14 +100,10 @@ async function retrieveLocationData(htmlContent, url) {
     if (!country)
     {
         country = findCountry(text, countries);
-    } else {
-        countryGotFromURL = true;
     }
 
     // Extract postcode
     let postcodeObject;
-    let parseAPIsuccesful = false;
-    let zipcodebaseAPIsuccesful = false;
 
     postcodeObject = await loopForPostcodeIfCountry(text, getPostalCodeFormat(country), country, getCountryAbbreviation(country),null, $, axios);  
 
@@ -164,18 +134,19 @@ async function retrieveLocationData(htmlContent, url) {
     console.log('Postcode:', postcode);
     console.log('Road:', road);
 
-    // Write to CSV
-    let data = [{
-        domain: url,
-        country: country,
-        region: region,
-        city: city,
-        postcode: postcode,
-        road: road,
-        roadNumber: roadNumber
+    return {country, region, city, postcode, road, roadNumber};
+}
+
+function writeCSV(data, domain) {
+    let dataObject = [{
+        domain: domain,
+        country: data?.country,
+        region: data?.region,
+        city: data?.city,
+        postcode: data?.postcode,
+        road: data?.road,
+        roadNumber: data?.roadNumber
     }];
 
-    csvWriter.writeRecords(data);
-
-    return {country, region, city, postcode, road, countryGotFromURL};
+    csvWriter.writeRecords(dataObject);
 }
