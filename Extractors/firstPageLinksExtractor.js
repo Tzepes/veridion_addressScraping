c18e4022ccdca5adad474a5bb9d0c5caf14a6d9f
Tzepes <andreicristian.vlad@gmail.com>
2024-05-07 15:00:14 +0300

    added list of irrelevant routes for link loop accessing

--- Extractors/firstPageLinksExtractor.js
+++ Extractors/firstPageLinksExtractor.js
@@ -1,15 +1,13 @@
 const url = require('url');
 
 async function getFirstPageLinks(domain, htmlContent, $) {
-    let links = new Set();
+    let links = [];
     const aTags = $('a');
     const unwantedLinkRegex = /^#|javascript:|mailto:|tel:|ftp:|data:|\.(pdf|jpg|png|mp3|mp4)$/;
     const socialMediaRegex = /twitter\.com|facebook\.com|instagram\.com|linkedin\.com|youtube\.com|pinterest\.com|patreon\.com|snapchat\.com/;
+    const ignoredRoutes = ['shop', 'product', 'news', 'media', 'services'];
 
     aTags.each((i, el) => {
-        // if (links.size >= 20) {
-        //     return false; // stop iteration
-        // }
         let link = $(el).attr('href');
         console.log(link);
         if (link && !unwantedLinkRegex.test(link) && !socialMediaRegex.test(link)) {
@@ -19,12 +17,21 @@ async function getFirstPageLinks(domain, htmlContent, $) {
             }
             // Resolve all links against the domain, whether they're relative or absolute
             link = url.resolve(domain, link);
-            links.add(link);
+
+            if (!ignoredRoutes.some(route => url.parse(link).pathname.includes(route))) {
+                links.push(link);
+            }
         }
     });
 
-    console.log(Array.from(links));
-    return Array.from(links);
+    const first10Links = links.slice(0, 10);
+    const last10Links = links.slice(-10);
+    const restOfLinks = links.slice(10, -10);
+
+    links = first10Links.concat(last10Links, restOfLinks);
+
+    console.log(links);
+    return links;
 }
 
 module.exports = getFirstPageLinks;
