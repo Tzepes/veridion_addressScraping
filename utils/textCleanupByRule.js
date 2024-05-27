4007a8451d7003d2f2bb2d63647b3f0ac8be7218
Tzepes <andreicristian.vlad@gmail.com>
2024-05-27 20:38:25 +0300

    shorten texts by country rule

---
+++ utils/textCleanupByRule.js
@@ -0,0 +1,21 @@
+const rulesByCountry = {
+    "US": {
+        minNumberOfTokens: 6,
+        maxNumberOfTokens: 15,
+        takeAmmountOfTokens: 10,
+    },
+    "UK": {
+
+    },
+    "DE": {
+        minNumberOfTokens: 3,
+        maxNumberOfTokens: 6,
+        takeAmmountOfTokens: 3,
+    },
+}
+
+function getTokenRules(country) {
+    return rulesByCountry[country];
+}
+
+module.exports = {getTokenRules};
